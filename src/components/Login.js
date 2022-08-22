7ad4545a11f0b24ae6b492830b282015e37adab1
Milly Arkwright <millyarkwright@Millys-MacBook-Air.local>
2022-08-22 16:12:59 +0100

    Login Event Error Fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,24 +20,24 @@ const Login = () => {
     event.preventDefault()
 
     try {
-      const response = await axios.post("http://localhost:4500/login", userData)
+      const response = await axios.post("http://localhost:4000/login", userData)
       const { token } = response.data
       localStorage.setItem("token", token)
       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
       navigate("/dashboard")
     } catch (error) {
+      console.log(error.message)
       setIsError(true)
-      setErrorMessage(error.response.data.message)
-      console.log(error)
+      setErrorMessage(error.message)
     }
   }
 
   return (
     <>
       <h1>Login for your account</h1>
-      <form onSubmit={() => handleSubmit(event)} action="">
-        <input type="text" name="username" placeholder="Your username" value={userData.username} onChange={() => handleFieldChange(event)}/>
-        <input type="password" name="password" placeholder="Your password" value={userData.password} onChange={() => handleFieldChange(event)} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder="Your username" value={userData.username} onChange={handleFieldChange}/>
+        <input type="password" name="password" placeholder="Your password" value={userData.password} onChange={handleFieldChange} />
         <button type="submit">Log In</button>
         {isError && errorMessage}
       </form>
