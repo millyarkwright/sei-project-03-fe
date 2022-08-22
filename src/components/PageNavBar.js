5bce098a9d4d4c0ffa23e6ff3a95d19ad2ed4044
Milly Arkwright <millyarkwright@Millys-MacBook-Air.local>
2022-08-22 17:52:47 +0100

    basic navbar components added

--- src/components/PageNavBar.js
+++ src/components/PageNavBar.js
@@ -1,10 +1,20 @@
 import { Link } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 
 const PageNavBar = () => {
   return (
-    <h1>navbar</h1>
+    <Navbar expand='sm'>
+      <Container as='section'>
+        <Navbar.Brand as={Link} to='/'>🍿🎬🍿</Navbar.Brand> 
+        <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
+        <Navbar.Collapse id='basic-navbar-nav' className='justify-content-end'>
+          <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+          <Nav.Link as={Link} to='/register'>Register</Nav.Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
