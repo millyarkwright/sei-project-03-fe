7a8029b58ad2efa9a2e79b91df838844fc99c1e0
JRose117 <joerose123@hotmail.co.uk>
2022-08-24 15:57:29 +0100

    made a dynamic form - no error tests

--- src/components/movies/MovieMatch.js
+++ src/components/movies/MovieMatch.js
@@ -1,42 +1,62 @@
-// import {useState, useEffect} from 'react'
-// const Match = () => {
+import {useState, useEffect} from 'react'
+const Match = () => {
 
-//   // const [error, setError] = useState('')
+  // const [error, setError] = useState('')
 
-//   const [watchWith, setWatchWith] = useState( { username: '' })
+  const [watchWith, setWatchWith] = useState( [{ username: '' }])
 
-//   // const handleButtonClick = (event) => {
-//   //   console.log(event)
-//   // }
+  const handleButtonClick = (event) => {
+    event.preventDefault()
+    console.log('this button will navigate, but for now it console logs')  }
   
-//   // const handleFieldChange = (event) => {
-//   //   console.log(event.target.value)
-//   // }
+  const handleFieldChange = (count, event) => {
+    const addUsername = [...watchWith]
+    addUsername[count] = event.target.value
+    setWatchWith(addUsername)
+  }
 
-//   // const handleSubmit = (event) => {
-//   //   event.preventDefault()
-//   //   console.log(event)
-//   // }
+  const addMoreUsernames = () => {
+    const newUsername = {username: ''}
+    setWatchWith([...watchWith, newUsername])
+  }
 
-//   // ! JSX
+  const removeUsernames = (count) => {
+    const deleteUsername = [...watchWith]
+    deleteUsername.splice(count, 1)
+    setWatchWith(deleteUsername)
+}
 
-// return (
-//   <>
-//     <h1> landed at Match </h1>
-//     <div className='dynamicForm'>
-//       <form onSubmit={handleSubmit}>
-//         {watchWith.map((input, count) => {
-//           return(
-//             <div key={count}>
-//               <input name='username' placeholder ='User Name'/>
-//             </div>
-//           )
-//         })}
-//       </form>
-//     </div>
-//     <button name="addMatch" onClick={handleButtonClick} >Match With A Friend</button>
-//   </>
-// )
-// }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('this button will navigate, but for now it console logs')
+  }
 
-// export default Match
+  // ! JSX
+
+return (
+  <>
+    <h1> landed at Match </h1>
+    <div className='dynamicForm'>
+      <form onSubmit={handleSubmit}>
+        {watchWith.map((input, count) => {
+          return(
+            <div key={count}>
+              <input 
+                name='username' 
+                placeholder ='User Name' 
+                value ={input.username} 
+                onChange={event => handleFieldChange(event, count)}
+                />
+                <button name="removeUsers" onClick={() => removeUsernames(count)}> Remove This User </button>
+            </div>
+          )
+        })}
+      </form>
+      <button name="Submit" onClick={handleButtonClick} >Match With A Friend</button>
+      <button name="addUsers" onClick={addMoreUsernames}> Add More Users To Watch With </button>
+    </div>   
+  </>
+)
+}
+
+export default Match
