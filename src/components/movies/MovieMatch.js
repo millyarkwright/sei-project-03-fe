3905519b86baa89008de8ddd86f47a03194afd88
JRose117 <joerose123@hotmail.co.uk>
2022-08-24 17:21:32 +0100

    creating test for empty username and username doesn't exist

--- src/components/movies/MovieMatch.js
+++ src/components/movies/MovieMatch.js
@@ -1,27 +1,33 @@
 import {useState, useEffect} from 'react'
+import { API_URL } from "../../config.js"
+import axios from 'axios'
+
 const Match = () => {
 
   // const [error, setError] = useState('')
 
   const [watchWith, setWatchWith] = useState( [{ username: '' }])
+  const [userData, setUserData] = useState([])
+  const [error, setError] = useState([])
 
-  const handleButtonClick = (event) => {
-    event.preventDefault()
-    console.log('this button will navigate, but for now it console logs')  }
-  
-  const handleFieldChange = (count, event) => {
-    const addUsername = [...watchWith]
+
+  const handleFieldChange = (event, count) => {
+    let addUsername = [...watchWith]
     addUsername[count] = event.target.value
     setWatchWith(addUsername)
   }
 
   const addMoreUsernames = () => {
-    const newUsername = {username: ''}
+    console.log('watchWith', watchWith)
+    console.log('Object.values(watchWith)', Object.values(watchWith))
+    console.log('Object.values(watchWith).slice(-1)', Object.values(Object.values(watchWith).slice(-1)))
+    console.log('Object.values(watchWith).slice(-1) === false', Object.values(Object.values(watchWith).slice(-1) === false))
+    let newUsername = {username: ''}
     setWatchWith([...watchWith, newUsername])
   }
 
   const removeUsernames = (count) => {
-    const deleteUsername = [...watchWith]
+    let deleteUsername = [...watchWith]
     deleteUsername.splice(count, 1)
     setWatchWith(deleteUsername)
 }
@@ -30,6 +36,23 @@ const Match = () => {
     event.preventDefault()
     console.log('this button will navigate, but for now it console logs')
   }
+
+  console.log(watchWith)
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const { data } = await axios.get(`${API_URL}/profile`)
+        console.log('userdata', data)
+        setUserData(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getUserData()
+  }, [])
+
+
 
   // ! JSX
 
@@ -43,16 +66,18 @@ return (
             <div key={count}>
               <input 
                 name='username' 
-                placeholder ='User Name' 
+                placeholder ='UserName' 
                 value ={input.username} 
                 onChange={event => handleFieldChange(event, count)}
                 />
-                <button name="removeUsers" onClick={() => removeUsernames(count)}> Remove This User </button>
+                {/* {count !== 0 &&  */}
+                {count !==0 && <button name="removeUsers" onClick={() => removeUsernames(count)}> Remove This User </button>}
+                {/* } */}
             </div>
           )
         })}
       </form>
-      <button name="Submit" onClick={handleButtonClick} >Match With A Friend</button>
+      <button name="Submit" onClick={handleSubmit} >Match With A Friend</button>
       <button name="addUsers" onClick={addMoreUsernames}> Add More Users To Watch With </button>
     </div>   
   </>
