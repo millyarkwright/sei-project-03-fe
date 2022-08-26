41b98ec225895cbbbf453d6a3d79604c8584113c
Alex <1995aha@gmail.com>
2022-08-26 10:57:29 +0100

    error page styling

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -5,8 +5,11 @@ const NotFoundPage = () => {
     <div className="error-page">
         <h1>Sorry, the page you are looking for does not exist.</h1>
         <h2>Error 404</h2>
-        <h3>Don't have an account? Create one <a href="">here.</a></h3>
-        <h3>If you have an account already, log in <a href="">here.</a>  </h3>
+        <div className="error-page-more-info">
+          <h3>Don't have an account? Create one <a href="">here.</a></h3>
+          <h3>If you have an account already, log in <a href="">here.</a>  </h3>
+        </div>
+
     </div>
     </>
   )
