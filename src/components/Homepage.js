9de042b2ec0f6fefd78e49e7d1cbc38ebbd0749a
Alex <1995aha@gmail.com>
2022-08-26 11:59:05 +0100

    added homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { Container } from "react-bootstrap"
 // import {useState, useEffect} from 'react'
 // import { Navigate, useNavigate } from 'react-router-dom'
 // import { API_URL } from "../config.js"
@@ -7,16 +8,21 @@ import { Link } from "react-router-dom"
 const Homepage = () => {
 
   return (<>
-    <h1>WatchIt</h1>
-      <h2>Decide What To Watch With Friends</h2>
-      <p>For our SEI 65 project 3, we decided to create a Tinder-like app that allows you to swipe on movies and connect with your friends to avoid the time-consuming task of trying to pick a movie that everyone likes. </p>
-    <h3>What’s the problem? </h3>
-    <p>We’ve all been there – you have some friends over, or maybe you have a chill weekend evening with your significant other, or you want to watch something new with your family, but nobody can agree on something that everyone would like to watch. It could be a movie, TV series or a documentary.</p>
-    <p>The task of deciding what to watch can take up lot of time and ruin the moment. </p>
-    <h3>How does WatchIt solve this problem? </h3>
-    <p>With WatchIt, you and movie mates can create an account, swipe on movies to watch and in the end, we will show you only the movies that everyone fancied. </p>
-    <p>This way you can go straight to enjoying whatever you liked the look of and never fear movie picking ever again.</p>
+    <main>   
+      
+      <div className="homepage-text">
+        <h1 className="watchit-title" >WatchIt</h1>
+        <h2>Decide What To Watch With Friends</h2>
+        <p>For our SEI 65 project 3, we decided to create a Tinder-like app that allows you to swipe on movies and connect with your friends to avoid the time-consuming task of trying to pick a movie that everyone likes. </p>
+        <h3>What’s the problem? </h3>
+        <p>We’ve all been there – you have some friends over, or maybe you have a chill weekend evening with your significant other, or you want to watch something new with your family, but nobody can agree on something that everyone would like to watch. It could be a movie, TV series or a documentary.</p>
+        <p>The task of deciding what to watch can take up lot of time and ruin the moment. </p>
+        <h3>How does WatchIt solve this problem? </h3>
+        <p>With WatchIt, you and movie mates can create an account, swipe on movies to watch and in the end, we will show you only the movies that everyone fancied. </p>
+        <p>This way you can go straight to enjoying whatever you liked the look of and never fear movie picking ever again.</p>
+      </div> 
 
+  </main>
   </>)
 }
 
--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -6,8 +6,8 @@ const NotFoundPage = () => {
         <h1>Sorry, the page you are looking for does not exist.</h1>
         <h2>Error 404</h2>
         <div className="error-page-more-info">
-          <h3>Don't have an account? Create one <a href="">here.</a></h3>
-          <h3>If you have an account already, log in <a href="">here.</a>  </h3>
+          <h3>Don't have an account? Create one <a href="/register">here.</a></h3>
+          <h3>If you have an account already, log in <a href="/login">here.</a>  </h3>
         </div>
 
     </div>
